941234d4b5dd41e9c0cf7ab3648687016a4e1ac4
Thalys Viana <thalysvianaguiar@gmail.com>
2022-10-27 03:32:29 -0300

    Add HPService to fetch info from the HP api

---
+++ HPSample/CharacterList/HPService.swift
@@ -0,0 +1,28 @@
+//
+//  HPService.swift
+//  HPSample
+//
+//  Created by Thalys Viana on 27/10/22.
+//
+
+import Foundation
+
+protocol HPServiceProtocol {
+  func fetchCharacters() async -> Characters
+}
+
+final class HPService: HPServiceProtocol {
+  private let service: NetworkServiceProtocol
+  
+  init(service: NetworkServiceProtocol = NetworkService()) {
+    self.service = service
+  }
+  
+  func fetchCharacters() async -> Characters {
+    do {
+      return try await service.fetchData(of: Characters.self, from: "https://hp-api.herokuapp.com/api/characters")
+    } catch {
+      return []
+    }
+  }
+}
