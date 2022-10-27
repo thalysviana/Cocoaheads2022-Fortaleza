8e95f5af9212ec7dd4a9716f43cca9434ace7ace
Thalys Viana <thalysvianaguiar@gmail.com>
2022-10-27 04:18:29 -0300

    Format file

--- HPSample/CharacterList/HPService.swift
+++ HPSample/CharacterList/HPService.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 protocol HPServiceProtocol {
-  func fetchCharacters() async -> Characters
+    func fetchCharacters() async -> Characters
 }
 
 final class HPService: HPServiceProtocol {
-  private let service: NetworkServiceProtocol
-  
-  init(service: NetworkServiceProtocol = NetworkService()) {
-    self.service = service
-  }
-  
-  func fetchCharacters() async -> Characters {
-    do {
-      return try await service.fetchData(of: Characters.self, from: "https://hp-api.herokuapp.com/api/characters")
-    } catch {
-      return []
+    private let service: NetworkServiceProtocol
+    
+    init(service: NetworkServiceProtocol = NetworkService()) {
+        self.service = service
     }
-  }
+    
+    func fetchCharacters() async -> Characters {
+        do {
+            return try await service.fetchData(of: Characters.self, from: "https://hp-api.herokuapp.com/api/characters")
+        } catch {
+            return []
+        }
+    }
 }
